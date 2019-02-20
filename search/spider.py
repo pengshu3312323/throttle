f39ea4680ffc3e3141d7886de7ce454bdef680ce
pencil <422870441@qq.com>
2019-02-20 15:47:26 +0800

    test

--- search/spider.py
+++ search/spider.py
@@ -27,22 +27,20 @@ class GoogleSpider:
         results = selector.xpath('//div[@class="rc"]')
         item_list = []
 
-        # for res in results:
-        res = results[1]
-        res_str = etree.tostring(res, encoding='utf-8')
-        res_selector = etree.HTML(res_str)
-        title = res_selector.xpath('//h3[@class="LC20lb"]/text()')
-        source = res_selector.xpath('//div[@class="r"]/a/@href')
-        des = res_selector.xpath('//span[@class="st"]')
+        for res in results:
+            res_str = etree.tostring(res, encoding='utf-8')
+            res_selector = etree.HTML(res_str)
+            title = res_selector.xpath('//h3[@class="LC20lb"]/text()')
+            source = res_selector.xpath('//div[@class="r"]/a/@href')
+            des = res_selector.xpath('//span[@class="st"]')
 
-        item = dict()
-            # search_type='google',
-            # keyword=keyword,
-            # page_num=pn
-            # )
-        item['title'] = title
-        item['source'] = source
-        item['des'] = etree.tostring(des).decode('utf-8')
+            item = dict()
+            item['search_type'] = 'g'
+            item['keyword'] = keyword
+            item['page_num'] = pn
+            item['title'] = title
+            item['source'] = source
+            item['des'] = etree.tostring(des).decode('utf-8')
 
-        item_list.append(item)
+            item_list.append(item)
         return item_list
